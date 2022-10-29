fc4b24d3961c7bf6a6ba188827cd8d7e2bda11df
Leosonicx <leonardocarmo941@gmail.com>
2022-10-29 18:26:40 -0300

    Diminuindo a qualidade do GIF

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,20 @@
 const offset = 0
-const limit = 150
+const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 fetch(url)
-  .then(function (response) {
-   console.log(response)
-})
-.catch(function (error) {
-  console.error(error)
-})
-.finally(function () {
-  console.log('Requisição concluída!')
-})
+  .then((response) => {
+    return response.json()
+  })
 
+  .then((jsonBody) => {
+    console.log(jsonBody)
+  })
+
+  .catch((error) => {
+    console.error(error)
+  })
+
+  .finally(() => {
+    console.log('Requisção concluída!')
+  })
