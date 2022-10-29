7173dd48138b4647a5cacf273e3139d90b5f2849
Leosonicx <leonardocarmo941@gmail.com>
2022-10-29 15:30:06 -0300

    Colocando redes sociais

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,15 @@
 const offset = 0
 const limit = 150
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${10}`
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function (response) {
-  console.log(response)
+fetch(url)
+  .then(function (response) {
+   console.log(response)
 })
+.catch(function (error) {
+  console.error(error)
+})
+.finally(function () {
+  console.log('Requisição concluída!')
+})
+
