0145c346e58b15a45c969d78f14feecdf4f25d40
Leosonicx <leonardocarmo941@gmail.com>
2022-10-28 23:24:16 -0300

    Atualização Pokedex
    Adicionando mais estilos.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
-console.log('Sucesso!')
+const offset = 0
+const limit = 150
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${10}`
+
+fetch(url).then(function (response) {
+  console.log(response)
+})
